ebb8a320afa4f4098ef0b738593bb55f71b60c32
HyunJoo Nam <hvl2042@gepplus.co.kr>
2025-06-13 09:47:20 +0900

    ✨ Update Title components

--- src/components/best/Title/Title.jsx
+++ src/components/best/Title/Title.jsx
@@ -1,18 +1,24 @@
 import React, { useState } from "react";
 import Tooltip from "../Tooltip/Tooltip";
+import useBreakpoint from "../../../hook/useBreakpoint";
 
 const Title = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const breakpoint = useBreakpoint();
 
   return (
-    <div className="box__best-title">
-      <h2 className="text__title">G마켓 베스트</h2>
-      <Tooltip
-        isOpen={isOpen}
-        onClick={() => setIsOpen(!isOpen)}
-        onClose={() => setIsOpen(false)}
-      />
-    </div>
+    <>
+      {breakpoint === "desktop" && (
+        <div className="box__best-title">
+          <h2 className="text__title">G마켓 베스트</h2>
+          <Tooltip
+            isOpen={isOpen}
+            onClick={() => setIsOpen(!isOpen)}
+            onClose={() => setIsOpen(false)}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
