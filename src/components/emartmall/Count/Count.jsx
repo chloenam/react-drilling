085cc7a1c19d689bb1cb9cfbe75c8d38f20812d1
HyunJoo Nam <hvl2042@gepplus.co.kr>
2025-06-18 10:05:10 +0900

    ✨ Update Count component

--- src/components/emartmall/Count/Count.jsx
+++ src/components/emartmall/Count/Count.jsx
@@ -4,23 +4,30 @@ const Count = () => {
   const [count, setCount] = useState(1);
 
   const decrease = () => {
-    setCount((prev) => (prev > 1 ? prev - 1 : prev));
+    setCount((current) => (current > 1 ? current - 1 : current));
   };
 
   const increase = () => {
-    setCount((prev) => prev + 1);
+    setCount((current) => current + 1);
   };
 
   return (
     <div className="element-info-qty">
-      <button aria-describedby="itemCard_title_2519000890" className="sprite__smilefresh--before element-info-qty-minus" aria-disabled="true" onClick={decrease}>
+      <button
+        className="sprite__smilefresh--before element-info-qty-minus"
+        aria-disabled="true"
+        onClick={decrease}
+      >
         <span className="for-a11y">상품 수 1 감소</span>
       </button>
-      <span className="element-info-item-qty" aria-describedby="itemCard_title_2519000890">
+      <span className="element-info-item-qty">
         <span className="for-a11y">선택된 수량</span>
         {count}
       </span>
-      <button aria-describedby="itemCard_title_2519000890" className="sprite__smilefresh--before element-info-qty-plus" onClick={increase}>
+      <button
+        className="sprite__smilefresh--before element-info-qty-plus"
+        onClick={increase}
+      >
         <span className="for-a11y">상품 수 1 증가</span>
       </button>
     </div>
