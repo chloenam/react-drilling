a61b69710532de9afb89c3c4eba561d95cc1e06f
chloenam <chloe.namhjoo@gmail.com>
2025-06-23 19:14:47 +0900

    💄 Update Tab items, add tab style

--- src/components/Tab/Tab.jsx
+++ src/components/Tab/Tab.jsx
@@ -21,6 +21,46 @@ const Tab = () => {
       >
         2week
       </NavLink>
+      <NavLink
+        to="/3week"
+        className={({ isActive }) =>
+          isActive ? "link__tab is-active" : "link__tab"
+        }
+      >
+        3week
+      </NavLink>
+      <NavLink
+        to="/4week"
+        className={({ isActive }) =>
+          isActive ? "link__tab is-active" : "link__tab"
+        }
+      >
+        4week
+      </NavLink>
+      <NavLink
+        to="/5week"
+        className={({ isActive }) =>
+          isActive ? "link__tab is-active" : "link__tab"
+        }
+      >
+        5week
+      </NavLink>
+      <NavLink
+        to="/6week"
+        className={({ isActive }) =>
+          isActive ? "link__tab is-active" : "link__tab"
+        }
+      >
+        6week
+      </NavLink>
+      <NavLink
+        to="/7week"
+        className={({ isActive }) =>
+          isActive ? "link__tab is-active" : "link__tab"
+        }
+      >
+        7week
+      </NavLink>
     </div>
   );
 };
